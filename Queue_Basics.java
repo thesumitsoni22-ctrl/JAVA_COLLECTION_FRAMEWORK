fc4e9e82579d541f128a9cec36dda86db7562e8b
Sumit Soni <thesumitsoni22@gmail.com>
2026-09-25 04:14:07 +0530

    Add Arraylist initialization,addition, and sorting techniques.

---
+++ Queue_Basics.java
@@ -0,0 +1,20 @@
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queue_Basics {
+    public static void main(String[] args) {
+
+//        Queue<Integer> q = new LinkedList<>();
+//        to use methods like addfirst(), addlast() must replace the Queue with Deque
+            Deque<Integer> q = new LinkedList<>();
+            q.offer(10); // add elements to the queuq
+            q.offer(20);
+            q.offer(30);
+        System.out.println(q);
+
+        System.out.println("Peeking : " + q.peek()); // returns the head element
+        q.poll();
+        System.out.println(q); // returns and remove the head element
+    }
+}
